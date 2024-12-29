c3fef9507f39272cced579cfc7785e04b6253380
bouni <bouni@owee.de>
2024-12-29 20:52:33 +0100

    Use product.json from Github

--- tiptoi-dl.py
+++ tiptoi-dl.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python
 
-import re
 import sys
 import requests
-from urllib.parse import unquote
 from pathlib import Path
 import psutil
-from bs4 import BeautifulSoup
+from tqdm import tqdm
 
+class DownloadProgressBar(tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
 
 class TipToiDL:
     def __init__(self):
-        self.base_url = (
-            "https://service.ravensburger.de/tiptoi%C2%AE/tiptoi%C2%AE_Audiodateien"
+        self.catalog_url = (
+            "https://bouni.github.io/tiptoi-dl/products.json"
         )
-        self.catalog = []
-        self.results = []
+        self.products = []
+        self.download_catalog()
         self.main()
+
+    def download_catalog(self):
+        r = requests.get(self.catalog_url, timeout=10)
+        self.products = r.json()
 
     def main(self):
         print("=" * 80)
         print("\nWelcome to TipToi-dl, a CLI TipToi audio file downloader.\n")
         print("=" * 80)
-        print("  Downloading Catalog ...")
-        self.get_catalog()
-        print(f"  Found {len(self.catalog)} files\n")
         while True:
             self.results = []
-            s = input("Enter search term or 'q' to quit: ")
+            s = input("\nEnter search term or 'q' to quit: ")
             if s == "q":
                 break
             if s != "":
                 self.search(s)
             if len(self.results) == 0:
-                print("  No results found")
+                print("\n  No results found")
                 continue
-            print(f"  Found {len(self.results)} results!\n")
+            print(f"\n  Found {len(self.results)} results!\n")
             for n, r in enumerate(self.results, 1):
                 print(f"  [{n}] {r["title"]}")
             print("\n")
@@ -45,83 +49,41 @@ class TipToiDL:
             try:
                 n = int(s)
             except ValueError:
-                print("  Not a vaild selection, quit ...")
+                print("\n  Not a vaild selection, quit ...")
                 sys.exit(1)
             if n < 0 or n > len(self.results):
-                print("  Not a valid selection, quit ...")
+                print("\n  Not a valid selection, quit ...")
                 break
-            print(f"  Your selection: {self.results[n-1]['title']}")
-            self.get_product_page(self.results[n - 1])
+            print(f"\n  Your selection: {self.results[n-1]['title']}")
+            self.download(self.results[n-1]["gme"])
 
-    def get_catalog(self):
-        r = requests.get(self.base_url)
-        soup = BeautifulSoup(r.text, "html.parser")
-        links = soup.find_all("a", class_="mt-listing-detailed-subpage-title")
-        for link in links:
-            self.catalog.append({"title": link.get("title"), "url": link.get("href")})
-
-    def get_product_page(self, product: dict):
-        r = requests.get(product["url"])
-        soup = BeautifulSoup(r.text, "html.parser")
-        links = soup.find_all(
-            "a",
-            href=re.compile("\.gme"),
-        )
-        subresults = []
-        if len(links) == 1:
-            try: 
-                title = links[0].find_next("p").span.strong.string
-            except:
-                title = links[0].find_next("p").strong.string
-            print(f"  Download {title} audio file now")
-            self.download(links[0].get("href"))
-        else:
-            print("  This product has multiple variants")
-            for n, link in enumerate(links, 1):
-                tr = link.parent.parent.find_next("tr")
-                td = tr.find_all("td")
-                title = "undefined"
-                try:
-                    title = td[n - 1].strong.string
-                except:
-                    pass
-                subresults.append({"title": title, "url": link.get("href")})
-                print(f"  [{n}] {title}")
-            print("\n")
-            s = input("Select item by entering number, q to quit: ")
-            if s == "q":
-                sys.exit()
-            try:
-                n = int(s)
-            except ValueError:
-                print("  Not a vaild selection, quit ...")
-                sys.exit(1)
-            if n < 0 or n > len(subresults):
-                print("  Not a valid selection, quit ...")
-                sys.exit()
-            print(f"  Your selection: {subresults[n-1]['title']}")
-            print(f"  Download {subresults[n-1]['title']} audio file now")
-            self.download(subresults[n - 1]["url"])
 
     def search(self, searchterm: str):
-        for item in self.catalog:
+        for item in self.products:
             if searchterm.lower() in item["title"].lower():
                 self.results.append(item)
 
     def download(self, url: str):
-        print(f"  Download {url}")
+        print(f"\n  Download {url}")
         filename = url.split("/")[-1]
-        filename = unquote(filename).replace(" ", "_")
         disk = self.find_tiptoi_disk()
         if not disk:
             path = Path.home() / filename
         else:
             path = Path(disk) / filename
         print(f"  Download file to {path}")
-        with requests.get(url, stream=True) as r:
-            with open(path, mode="wb") as f:
-                f.write(r.content)
-        sys.exit()
+        resp = requests.get(url, stream=True)
+        total = int(resp.headers.get('content-length', 0))
+        with open(path, 'wb') as file, tqdm(
+            desc=filename,
+            total=total,
+            unit='iB',
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as bar:
+            for data in resp.iter_content(chunk_size=1024):
+                size = file.write(data)
+                bar.update(size)
 
     def find_tiptoi_disk(self):
         disks = [
