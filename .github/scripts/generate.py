e80b22dccbe3ac82b61bd9279c23d7f7aa7c12ca
bouni <bouni@owee.de>
2024-12-29 19:59:42 +0100

    flush

--- .github/scripts/generate.py
+++ .github/scripts/generate.py
@@ -16,7 +16,7 @@ class TipToiCatalog:
         )
         self.catalog = []
         self.products = []
-        print("Start data scraping")
+        print("Start data scraping", flush=True)
         self.get_catalog()
         for n, product in enumerate(self.catalog):
             self.products.append(self.get_product_data(product))
@@ -25,10 +25,10 @@ class TipToiCatalog:
 
     def get_catalog(self):
         r = requests.get(self.base_url, timeout=10)
-        print(f"Status code: {r.status_code}")
+        print(f"Status code: {r.status_code}", flush=True)
         soup = BeautifulSoup(r.text, "html.parser")
         links = soup.find_all("a", class_="mt-listing-detailed-subpage-title")
-        print(f"Found {len(links)} links")
+        print(f"Found {len(links)} links", flush=True)
         for link in links:
             self.catalog.append({"title": link.get("title"), "url": link.get("href")})
 
@@ -56,11 +56,11 @@ class TipToiCatalog:
     def get_product_data(self, product: dict) -> dict:
         product = self.get_product_numbers(product)
         product = self.sanitize_title(product)
-        print(f"Get product data for {product['title']}")
+        print(f"Get product data for {product['title']}", flush=True)
         product_data = {}
 
         r = requests.get(product["url"], timeout=10)
-        print(f"Status code: {r.status_code}")
+        print(f"Status code: {r.status_code}", flush=True)
 
         soup = BeautifulSoup(r.text, "html.parser")
         links = soup.find_all(
@@ -76,7 +76,7 @@ class TipToiCatalog:
         return product_data
 
     def persist_products(self):
-        print("Write json file")
+        print("Write json file", flush=True)
         (BASEPATH / "output").mkdir(exist_ok=True)
         with open(BASEPATH / "output/products.json", "w") as f:
             json.dump(self.products, f)
